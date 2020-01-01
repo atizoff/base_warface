8fe7e9fdd4d04ea4ed744e405d3ae9bd91d9b70f
Elshad <42542773+atiZZZ@users.noreply.github.com>
2020-01-02 01:39:39 +0300

    hook
    again fix. thanks, Darknz

--- base_warface/dllmain.cpp
+++ base_warface/dllmain.cpp
@@ -3,16 +3,16 @@
 #include "cFunction.h"
 #include "vmt.h"
 
-typedef __int64 (WINAPI* EndSceenFn)(IDirect3DDevice9*, CONST RECT*, CONST RECT*, HWND, CONST RGNDATA*);
-EndSceenFn pEndSceen;
+typedef __int64 (WINAPI* PresentSceneFN)(IDirect3DDevice9*, CONST RECT*, CONST RECT*, HWND, CONST RGNDATA*);
+PresentSceneFN oPresent;
 
 
 VMTHookManager* VmtHook = new VMTHookManager;
 
-HRESULT WINAPI myEndSceen(IDirect3DDevice9* m_pDevice, CONST RECT* pSourceRect, CONST RECT* pDestRect, HWND hDestWindowOverride, CONST RGNDATA* pDirtyRegion)
+HRESULT WINAPI PresentScene(IDirect3DDevice9* m_pDevice, CONST RECT* pSourceRect, CONST RECT* pDestRect, HWND hDestWindowOverride, CONST RGNDATA* pDirtyRegion)
 {
 	function();
-	return pEndSceen(m_pDevice, pSourceRect, pDestRect, hDestWindowOverride, pDirtyRegion);
+	return oPresent(m_pDevice, pSourceRect, pDestRect, hDestWindowOverride, pDirtyRegion);
 }
 
 BOOL WINAPI DllMain(HMODULE hModule, DWORD64 dwReason, LPVOID)
@@ -21,7 +21,7 @@ BOOL WINAPI DllMain(HMODULE hModule, DWORD64 dwReason, LPVOID)
 	{
 	case DLL_PROCESS_ATTACH:
 		VmtHook->bInitialize((PDWORD64*)SSystemGlobalEnvironment::Singleton()->GetIRenderer()->GetDirectDevice());
-		pEndSceen = (EndSceenFn)VmtHook->dwHookMethod((DWORD64)myEndSceen, 42);
+		pEndSceen = (EndSceenFn)VmtHook->dwHookMethod((DWORD64)PresentScene, 17);
 		break;
 
 	case DLL_PROCESS_DETACH:
